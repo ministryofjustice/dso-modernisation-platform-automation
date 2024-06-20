3626c03c08a6e0dcc4dce7184d3a97e13269a08c
Dominic Robinson <dominic.robinson@digital.justice.gov.uk>
2024-06-20 14:26:55 +0100

    fix

--- src/ami_cleanup.sh
+++ src/ami_cleanup.sh
@@ -5,7 +5,35 @@
 set -eo pipefail
 
 usage() {
-  echo "Usage $0: ec2|ami|list|delete <months>"
+  echo "Usage $0: ec2|ami|code|list|delete <months>
+ 
+$0 ec2
+  - list all AMIs referenced by EC2s
+
+$0 ami <months>
+  - list all AMIs in account older than <months>
+
+$0 code_names <application>
+  - list all AMI names found in the environments repo, optionally provide an application name, e.g. nomis
+
+$0 code <application>
+  - list all AMIs in account which are also referenced in the environments repo, optionally provide an application name, e.g. nomis
+
+$0 ec2_and_code <application>
+  - list all AMIs either referenced by EC2s or referenced in environments repo 
+
+$0 list <months> [keep_ec2_and_code <application>]
+  - list all AMIs older than <months> that can be deleted. By default just check for AMIs referenced by running EC2s. But option to keep those referenced in code also
+
+$0 list_including_backup <months> [keep_ec2_and_code <application>]
+  - same as list except includes any AwsBackup AMIs found
+
+$0 delete <months> [keep_ec2_and_code <application>]
+  - delete all AMIs older than <months> that can be deleted. By default just check for AMIs referenced by running EC2s. But option to keep those referenced in code also
+
+$0 delete_including_backup <months> [keep_ec2_and_code <application>]
+  - same as delete except includes any AwsBackup AMIs found
+"
 }
 
 date_minus_month() {
@@ -55,7 +83,7 @@ get_date_filter() {
 
 get_images_csv() {
   # Input: <months_history>
-  # Output: OwnerId,ImageId,Name,CreationDate,Public
+  # Output: ImageId,OwnerId,CreationDate,Public,Name
   local date_filter
   local json 
 
@@ -73,7 +101,8 @@ get_images_csv() {
   jq -r ".Images[] | [.ImageId, .OwnerId, .CreationDate, .Public, .Name] | @csv" <<< $json | sed 's/"//g'
 }
 
-get_ec2_instance_images() {
+get_ec2_instance_images_csv() {
+  # Output: ImageId,OwnerId,CreationDate,Public,Name
   local ids
   local json 
 
@@ -85,19 +114,91 @@ get_ec2_instance_images() {
   jq -r ".Images[] | [.ImageId, .OwnerId, .CreationDate, .Public, .Name] | @csv" <<< $json | sed 's/"//g'
 }
 
-if [[ $1 == "ec2" ]]; then
-  get_ec2_instance_images | sort -t, -k3
-elif [[ $1 == "ami" ]]; then
-  get_images_csv $2 | sort -t, -k3
-elif [[ $1 == "list" ]]; then
-  ec2=$(get_ec2_instance_images | sort)
-  ami=$(get_images_csv $2 | sort)
-  comm -23 <(echo "$ami") <(echo "$ec2") | sort -t, -k3
-elif [[ $1 == "delete" ]]; then
-  ec2=$(get_ec2_instance_images | sort)
-  ami=$(get_images_csv $2 | sort)
-  comm -23 <(echo "$ami") <(echo "$ec2") | sort -t, -k3
-else
-  usage >&2
-  exit 1
-fi
+get_code_image_names() {
+  local envdir
+
+  if [[ -z $1 ]]; then
+    envdir="$(dirname $0)/../../modernisation-platform-environments/terraform/environments"
+    if [[ ! -d "$envdir" ]]; then
+      echo "Cannot find $envdir" >&2
+      exit 1
+    fi
+    grep -Eo 'ami_name[[:space:]]*=[[:space:]]*"[^"]*"' $envdir/*/*.tf | cut -d\" -f2 | sort -u | grep -v '*' | sort -u   
+  else
+    envdir="$(dirname $0)/../../modernisation-platform-environments/terraform/environments/$1"
+    if [[ ! -d "$envdir" ]]; then
+      echo "Cannot find $envdir" >&2
+      exit 1
+    fi
+    grep -Eo 'ami_name[[:space:]]*=[[:space:]]*"[^"]*"' $envdir/*.tf | cut -d\" -f2 | sort -u | grep -v '*' | sort -u   
+  fi
+}
+
+get_code_csv() {
+  ami=$(get_images_csv 0 | sort -t, -k5)
+  code=$(get_code_image_names $1)
+  join -o 1.1,1.2,1.3,1.4,1.5  -t, -1 5 <(echo "$ami") <(echo "$code")
+}
+
+get_ec2_and_code_csv() {
+  ami=$(get_images_csv 0 | sort -t, -k5)
+  code=$(get_code_image_names $1)
+  amicode=$(join -o 1.1,1.2,1.3,1.4,1.5  -t, -1 5 <(echo "$ami") <(echo "$code") | sort)
+  ec2=$(get_ec2_instance_images_csv | sort)
+  comm <(echo "$amicode") <(echo "$ec2") | tr -d ' ' | tr -d '\t'
+}
+
+get_images_to_delete_csv() {
+  local ami
+  local ec2
+
+  ami=$(get_images_csv $1 | sort)
+  if [[ $2 == "keep_ec2_and_code" ]]; then
+    ec2=$(get_ec2_and_code_csv $3 | sort)
+  else 
+    ec2=$(get_ec2_instance_images_csv | sort)
+  fi
+  comm -23 <(echo "$ami") <(echo "$ec2")
+}
+
+delete_images() {
+  echo "DELETING FOLLOWING AMIS"
+  echo "${@}"
+  for id in $(echo "$@" | cut -d, -f1); do
+    echo aws ec2 deregister-image --image-id $id
+  done
+
+}
+
+main() {
+  if [[ $1 == "ec2" ]]; then
+    get_ec2_instance_images_csv | sort -t, -k3
+  elif [[ $1 == "ami" ]]; then
+    get_images_csv $2 | sort -t, -k3
+  elif [[ $1 == "code_names" ]]; then
+    get_code_image_names $2
+  elif [[ $1 == "code" ]]; then
+    get_code_csv $2  | sort -t, -k3
+  elif [[ $1 == "ec2_and_code" ]]; then
+    get_ec2_and_code_csv $2 | sort -t, -k3
+  elif [[ $1 == "list" ]]; then
+    shift
+    get_images_to_delete_csv $@ | grep -v AwsBackup | sort -t, -k3
+  elif [[ $1 == "list_including_backup" ]]; then
+    shift
+    get_images_to_delete_csv $@ | sort -t, -k3
+  elif [[ $1 == "delete" ]]; then
+    shift
+    csv=$(get_images_to_delete_csv $@ | grep -v AwsBackup |  sort -t, -k3)
+    delete_images "$csv"
+  elif [[ $1 == "delete_including_backup" ]]; then
+    shift
+    csv=$(get_images_to_delete_csv $@ | sort -t, -k3)
+    delete_images "$csv"
+  else
+    usage >&2
+    exit 1
+  fi
+}
+
+main "$@"
