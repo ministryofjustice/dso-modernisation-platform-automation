8f097cb3bbf4de7332119dc98919bf42b9f3aabb
Craig Gilbertson <crvgilbertson@gmail.com>
2024-07-16 10:15:53 +0100

    get names

--- src/nomis-combined-reporting/ncr_control.sh
+++ src/nomis-combined-reporting/ncr_control.sh
@@ -16,9 +16,9 @@ Where <opts>:
 
 get_instance_names() {
   local nomis_combined_reporting_environment
-  local cms_urls
-  local processing_urls
-  local tomcat_urls
+  local cms_names
+  local processing_names
+  local tomcat_names
 
   nomis_combined_reporting_environment=$1
   if [[ $nomis_combined_reporting_environment == "dev" ]]; then
@@ -29,6 +29,9 @@ get_instance_names() {
     account_name="nomis-combined-reporting-preproduction"
   elif [[ $nomis_combined_reporting_environment == "preprod" ]]; then
     account_name="nomis-combined-reporting-preproduction"
+    cms_names=("pp-ncr-cms-a.nomis-combined-reporting.hmpps-preproduction.modernisation-platform.service.justice.gov.uk", "pp-ncr-cms-b.nomis-combined-reporting.hmpps-preproduction.modernisation-platform.service.justice.gov.uk")
+    processing_names=("pp-ncr-processing-a.nomis-combined-reporting.hmpps-preproduction.modernisation-platform.service.justice.gov.uk")
+    tomcat_names=("pp-ncr-web-admin-a.nomis-combined-reporting.hmpps-preproduction.modernisation-platform.service.justice.gov.uk", "pp-ncr-web-a.nomis-combined-reporting.hmpps-preproduction.modernisation-platform.service.justice.gov.uk", "pp-ncr-web-b.nomis-combined-reporting.hmpps-preproduction.modernisation-platform.service.justice.gov.uk")
   elif [[ $nomis_combined_reporting_environment == "prod" ]]; then
     account_name="nomis-combined-reporting-production"
     cms_names=("pd-ncr-cms-a.nomis-combined-reporting.hmpps-preproduction.modernisation-platform.service.justice.gov.uk", "pd-ncr-cms-b.nomis-combined-reporting.hmpps-preproduction.modernisation-platform.service.justice.gov.uk")
@@ -37,37 +40,23 @@ get_instance_names() {
     return 1
   fi
   echo "$account_name"
-}
-
-get_instance_names() {
-  local nomis_combined_reporting_environment
-
-  nomis_combined_reporting_environment=$1
-  if [[ $nomis_combined_reporting_environment == "dev" ]]; then
-    account_name="nomis-combined-reporting-development"
-  elif [[ $nomis_combined_reporting_environment == "t1" ]]; then
-    account_name="nomis-combined-reporting-test"
-  elif [[ $nomis_combined_reporting_environment == "lsast" ]]; then
-    account_name="nomis-combined-reporting-preproduction"
-  elif [[ $nomis_combined_reporting_environment == "preprod" ]]; then
-    account_name="nomis-combined-reporting-preproduction"
-  elif [[ $nomis_combined_reporting_environment == "prod" ]]; then
-    account_name="nomis-combined-reporting-production"
-  else
-    echo "Unsupported nomis_combined_reporting_environment $nomis_combined_reporting_environment" >&2
-    return 1
-  fi
-  echo "$account_name"
+  echo "$cms_names"
+  echo "$processing_names"
+  echo "$tomcat_names"
 }
 
 start_ncr() {
   # Get environment
   # Get instance name for cms machines, processing nodes and tomcat servers
   # run start server bash script on cms machines a then b
-  # run stop server bash script on 
-
+  # run start server bash script on processing nodes
+  # run start server bash script on tomcat web servers
 }
 
 stop_ncr() {
-# Get instance name for cms machines
+  # Get environment
+  # Get instance name for cms machines, processing nodes and tomcat servers
+  # run stop server bash script on tomcat web servers
+  # run stop server bash script on processing nodes
+  # run stop server bash script on cms machines b then a
 }
