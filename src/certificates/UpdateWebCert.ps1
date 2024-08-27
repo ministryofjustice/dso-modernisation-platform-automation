da2f39efb5ced123ad14720a0f7e55cf0bd959a8
Dominic Robinson <dominic.robinson@digital.justice.gov.uk>
2024-08-27 10:14:58 +0100

    debug

--- src/certificates/UpdateWebCert.ps1
+++ src/certificates/UpdateWebCert.ps1
@@ -12,11 +12,13 @@ if (-not $WebBinding) {
 $ChainFileName = [System.IO.Path]::GetTempFileName().Replace(".tmp",".pem")
 [IO.File]::WriteAllBytes($ChainFileName, [Convert]::FromBase64String($ChainFileBase64))
 $CACert = Import-Certificate -FilePath $ChainFileName -CertStoreLocation 'Cert:\LocalMachine\CA'
+$CACert
 Remove-Item -Path $ChainFileName -Force
 
 $PfxFileName = [System.IO.Path]::GetTempFileName().Replace(".tmp",".pfx")
 [IO.File]::WriteAllBytes($PfxFileName, [Convert]::FromBase64String($PfxFileBase64))
 $PfxCert = Import-PfxCertificate -FilePath $PfxFileName -CertStoreLocation 'Cert:\LocalMachine\My' -Password $Password
+$PfxCert
 Remove-Item -Path $PfxFileName -Force
 
 $WebBinding = Get-WebBinding -Name 'Default Web Site' | Where-Object -Property protocol -eq 'https'
@@ -24,4 +26,5 @@ if (-not $WebBinding) {
   Write-Ouput "Creating https WebBinding"
   $WebBinding = New-WebBinding -Name $Config.WebSiteName -IPAddress "*" -Port 443 -Protocol "https"
 }
+$WebBinding
 $WebBinding.AddSslCertificate($PfxCert.Thumbprint, 'Cert:\LocalMachine\My')
