4e2bffaad17911bf1f2faac10ffcabb4ed625344
Dominic Robinson <dominic.robinson@digital.justice.gov.uk>
2024-08-27 14:33:23 +0100

    fix

---
+++ src/certificates/ImportLetsEncryptCert.ps1
@@ -0,0 +1,20 @@
+$ErrorActionPreference = "Stop"
+$ChainFileBase64 = "sed-chain-file"
+$PfxFileBase64 = "sed-pfx-file"
+$Password = "sed-password"
+$PasswordSecureString = ConvertTo-SecureString $Password -AsPlainText -Force
+
+$ChainFileName = [System.IO.Path]::GetTempFileName().Replace(".tmp",".pem")
+[IO.File]::WriteAllBytes($ChainFileName, [Convert]::FromBase64String($ChainFileBase64))
+$CACert = Import-Certificate -FilePath $ChainFileName -CertStoreLocation 'Cert:\LocalMachine\CA'
+$CACert
+Remove-Item -Path $ChainFileName -Force
+
+$PfxFileName = [System.IO.Path]::GetTempFileName().Replace(".tmp",".pfx")
+[IO.File]::WriteAllBytes($PfxFileName, [Convert]::FromBase64String($PfxFileBase64))
+$PfxCert = Import-PfxCertificate -FilePath $PfxFileName -CertStoreLocation 'Cert:\LocalMachine\My' -Password $PasswordSecureString
+$PfxCert
+Remove-Item -Path $PfxFileName -Force
+
+Thumbprint=$PfxCert.Thumbprint
+Write-Output "cert_thumbprint=$Thumbprint"
---
+++ src/certificates/UpdateWebBinding.ps1
@@ -0,0 +1,16 @@
+$ErrorActionPreference = "Stop"
+$DomainNames = "sed-domain-names"
+
+$WebBinding = Get-WebBinding -Name 'Default Web Site' | Where-Object -Property protocol -eq 'https'
+if (-not $WebBinding) {
+  Write-Output "Creating Default Web Site https WebBinding"
+  $WebBinding = New-WebBinding -Name 'Default Web Site' -IPAddress "*" -Port 443 -Protocol "https"
+}
+
+$Cert = Get-ChildItem -Path 'Cert:\LocalMachine\My' | Where-Object -Property 'Subject' -match ($DomainNames.split(' ')[0]) | Sort-Object NotAfter | Select-Object -Last 1
+if (-not $Cert) {
+  Write-Output "Could not find certificate in store"
+  Exit 1
+}
+
+$WebBinding.AddSslCertificate($Cert.Thumbprint, 'Cert:\LocalMachine\My')
