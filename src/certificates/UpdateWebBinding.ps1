883df1a2d6b7c35806945d0aad5c6b430b273e23
Dominic Robinson <dominic.robinson@digital.justice.gov.uk>
2024-08-28 15:50:02 +0100

    add debug

--- src/certificates/UpdateWebBinding.ps1
+++ src/certificates/UpdateWebBinding.ps1
@@ -4,7 +4,8 @@ $DomainNames = "sed-domain-names"
 $WebBinding = Get-WebBinding -Name 'Default Web Site' | Where-Object -Property protocol -eq 'https'
 if (-not $WebBinding) {
   Write-Output "Creating Default Web Site https WebBinding"
-  $WebBinding = New-WebBinding -Name 'Default Web Site' -IPAddress "*" -Port 443 -Protocol "https"
+  New-WebBinding -Name 'Default Web Site' -IPAddress "*" -Port 443 -Protocol "https"
+  $WebBinding = Get-WebBinding -Name 'Default Web Site' | Where-Object -Property protocol -eq 'https'
 }
 
 $Cert = Get-ChildItem -Path 'Cert:\LocalMachine\My' | Where-Object -Property 'Subject' -match ($DomainNames.split(' ')[0]) | Sort-Object NotAfter | Select-Object -Last 1
@@ -12,5 +13,6 @@ if (-not $Cert) {
   Write-Output "Could not find certificate in store"
   Exit 1
 }
-
+$Cert
+Write-Output "Updaing Default Web Site https WebBinding"
 $WebBinding.AddSslCertificate($Cert.Thumbprint, 'My')
