8808f65482f53fdcc7f04bd74afa7cb7a930ca9b
Dominic Robinson <65237317+drobinson-moj@users.noreply.github.com>
2024-11-19 14:45:30 +0000

    TM-718: GitHub monitoring improvements (#129)
    * script
    
    * update pipeline
    
    * test
    
    * fix
    
    * fix
    
    * tweak

---
+++ src/github-workflow-monitoring/github-workflow-monitor.py
@@ -0,0 +1,190 @@
+"""Get aggregate statistics for Scheduled Github Workflows"""
+
+import argparse
+import json
+import subprocess
+import datetime
+import os
+import textwrap
+import sys
+
+CURL_TIMEOUT_SECS = 10
+
+GITHUB_REPOS = [
+    "dso-certificates",
+    "dso-infra-azure-ad",
+    "dso-infra-azure-fixngo",
+    "dso-modernisation-platform-automation",
+    "dso-repositories",
+    "dso-useful-stuff",
+]
+
+def run_github_api(uri, github_token):
+    """Invoke curl to call API"""
+
+    cmd = [
+        'curl',
+        '-s',
+        '-H',
+        f'Authorization: Bearer {github_token}',
+        uri,
+    ]
+    result = subprocess.run(cmd,
+                            capture_output=True,
+                            text=True,
+                            check=False,
+                            timeout=CURL_TIMEOUT_SECS)
+    if result.returncode != 0:
+        raise ValueError(f'exit code {result.returncode} for {uri}' + os.linesep + result.stderr)
+    return json.loads(result.stdout)
+
+
+def add_workflow_stat(workflow_summary, repo, name, status):
+    """Add to the workflow stats"""
+
+    zeroed_stats = {}
+    zeroed_stats["success"] = 0
+    zeroed_stats["failed"] = 0
+    if 'all' not in workflow_summary:
+        workflow_summary['all'] = {}
+        workflow_summary['all']['all'] = zeroed_stats.copy()
+    if name not in workflow_summary['all']:
+        workflow_summary['all'][name] = zeroed_stats.copy()
+    if repo is not None:
+        if repo not in workflow_summary:
+            workflow_summary[repo] = {}
+            workflow_summary[repo]['all'] = zeroed_stats.copy()
+        if name not in workflow_summary[repo]:
+            workflow_summary[repo][name] = zeroed_stats.copy()
+    if status is not None:
+        workflow_summary['all']['all'][status] += 1
+        workflow_summary['all'][name][status] += 1
+        if repo is not None:
+            workflow_summary[repo]['all'][status] += 1
+            workflow_summary[repo][name][status] += 1
+
+
+def check_workflow_run(workflow_summary, repo, workflow_run, verbose, start_timestamp, end_timestamp):
+    """Parse workflow json and add to the workflow stats"""
+
+    path = workflow_run['path']
+    filename = path.split('/')[-1].split('.')[0]
+    status = workflow_run['status']
+    created_at = workflow_run['created_at']
+    run_number = workflow_run['run_number']
+
+    add_workflow_stat(workflow_summary, repo, filename, None)
+
+    if verbose >= 4:
+        sys.stderr.write(
+            textwrap.indent(json.dumps(workflow_run, indent=1),
+                            'Verbose4: ') + os.linesep)
+
+    if status != 'completed':
+        if verbose >= 2:
+            sys.stderr.write(
+                f'Verbose2: {repo} {filename}#{run_number} start={created_at}: ignoring {status}'
+                + os.linesep)
+        return
+
+    updated_at = workflow_run['updated_at']
+    timestamp = datetime.datetime.strptime(updated_at, '%Y-%m-%dT%H:%M:%S%z')
+
+    if timestamp < start_timestamp or timestamp >= end_timestamp:
+        return
+
+    conclusion = workflow_run['conclusion']
+    if conclusion in ['failure', 'timed_out']:
+        if verbose >= 1:
+            sys.stderr.write(
+                f'Verbose1: {repo} {filename}#{run_number} start={created_at} end={updated_at}: {conclusion}'
+                + os.linesep)
+        add_workflow_stat(workflow_summary, repo, filename, 'failed')
+    elif conclusion in ['success']:
+        if verbose >= 3:
+            sys.stderr.write(
+                f'Verbose3: {repo} {filename}#{run_number} start={created_at} end={updated_at}: {conclusion}'
+                + os.linesep)
+        add_workflow_stat(workflow_summary, repo, filename, 'success')
+    else:
+        if verbose >= 2:
+            sys.stderr.write(
+                f'Verbose2: {repo} {filename}#{run_number} start={created_at} end={updated_at}: ignoring {conclusion}'
+                + os.linesep)
+
+
+def print_workflow_summary_csv(workflow_summary, timestamp):
+    """print ssm workflow stats in csv"""
+
+    print('Timestamp,Repo,WorkflowName,SuccessCount,FailedCount')
+    for repo in workflow_summary.keys():
+        for name in workflow_summary[repo].keys():
+            workflow = workflow_summary[repo][name]
+            print(
+                f'{timestamp},{repo},{name},{workflow["success"]},{workflow["failed"]}'
+            )
+
+
+def main():
+    """Main function"""
+
+    parser = argparse.ArgumentParser(
+        description='Check Github Scheduled Workflow status')
+    parser.add_argument('-i',
+                        '--interval',
+                        required=True,
+                        type=int,
+                        help='Check SSM workflow for this time interval in seconds')
+    parser.add_argument('-r',
+                        '--round',
+                        action='store_true',
+                        help='Round the time interval checked, e.g. if 3600, check from 14:00 to 15:00')
+    parser.add_argument('-v',
+                        '--verbose',
+                        action='count',
+                        default=0,
+                        help='Verbose output')
+    parser.add_argument('repo',
+                        nargs='+',
+                        help='1 or more repo names. Set to all to use defaults from script')
+
+    args = parser.parse_args()
+
+    timestamp = datetime.datetime.now(datetime.timezone.utc)
+    timestamp = timestamp.replace(microsecond=0)
+    if args.round:
+        epoch_time = datetime.datetime(1970, 1, 1, tzinfo=datetime.timezone.utc)
+        delta = int((timestamp - epoch_time).total_seconds()) % args.interval
+        end_timestamp = timestamp - datetime.timedelta(seconds=delta)
+        start_timestamp = end_timestamp - datetime.timedelta(seconds=args.interval)
+        invoke_after_timestamp = start_timestamp - datetime.timedelta(seconds=60)
+    else:
+        start_timestamp = timestamp - datetime.timedelta(seconds=args.interval)
+        end_timestamp = timestamp
+        invoke_after_timestamp = start_timestamp
+    one_day_ago_timestamp = timestamp - datetime.timedelta(days=1)
+    invoke_after_timestamp = min(one_day_ago_timestamp, invoke_after_timestamp)
+
+    if os.environ.get('GITHUB_TOKEN') is None:
+        raise ValueError('please set GITHUB_TOKEN environment variable')
+    github_token = os.environ.get('GITHUB_TOKEN')
+
+    workflow_summary = {}
+    repos = args.repo
+    if repos is None or repos[0] == 'all':
+        repos = GITHUB_REPOS
+
+    for repo in repos:
+        date = invoke_after_timestamp.strftime("%Y-%m-%dT%TZ")
+        workflow_runs = run_github_api(f'https://api.github.com/repos/ministryofjustice/{repo}/actions/runs?event=schedule&updated=>={date}', github_token)
+        if 'workflow_runs' not in workflow_runs:
+            sys.stderr.write(json.dumps(workflow_runs, indent=1))
+            raise ValueError('API response error')
+
+        for workflow_run in workflow_runs['workflow_runs']:
+            check_workflow_run(workflow_summary, repo, workflow_run, args.verbose, start_timestamp, end_timestamp)
+
+    print_workflow_summary_csv(workflow_summary, end_timestamp.strftime("%Y-%m-%dT%TZ"))
+
+
+main()
---
+++ src/github-workflow-monitoring/github-workflow-monitor.sh
@@ -0,0 +1,132 @@
+#!/bin/bash
+# Get GitHub Action Run stats and upload to cloudwatch metrics
+# Don't forget to set your default AWS profile and github token
+# export AWS_DEFAULT_PROFILE=nomis-development
+# export GITHUB_TOKEN=xxxxx
+
+set -eo pipefail
+
+BASEDIR=$(dirname "$0")
+CLOUDWATCH=0
+DRYRUN=0
+INTERVAL=
+
+usage() {
+  echo "Usage $0: [<opts>] -i <interval> [<repo1>] .. [<repoN>]
+
+Where <opts>:
+  -c                     Upload metrics to cloudwatch
+  -d                     Dryrun - show cloudwatch aws cli commands but don't upload
+  -r                     Round the time interval checked
+"
+}
+
+put_metric_data_dimension() {
+  if [[ -z $4 ]]; then
+    if [[ $DRYRUN == 0 ]]; then
+      echo "aws cloudwatch put-metric-data --namespace CustomMetrics --metric-name '$1' --value '$2' --timestamp '$3' --region eu-west-2" >&2
+      aws cloudwatch put-metric-data --namespace CustomMetrics --metric-name "$1" --value "$2" --timestamp "$3" --region eu-west-2
+    else
+      echo "DRYRUN: aws cloudwatch put-metric-data --namespace CustomMetrics --metric-name '$1' --value '$2' --timestamp '$3' --region eu-west-2" >&2
+    fi
+  else
+    if [[ $DRYRUN == 0 ]]; then
+      echo "aws cloudwatch put-metric-data --namespace CustomMetrics --metric-name '$1' --value '$2' --timestamp '$3' --dimension '$4' --region eu-west-2" >&2
+      aws cloudwatch put-metric-data --namespace CustomMetrics --metric-name "$1" --value "$2" --timestamp "$3" --dimension "$4" --region eu-west-2
+    else
+      echo "DRYRUN: aws cloudwatch put-metric-data --namespace CustomMetrics --metric-name '$1' --value '$2' --timestamp '$3' --dimension '$4' --region eu-west-2" >&2
+    fi
+  fi
+}
+
+put_metric_data() {
+  local metric_name
+  local repo
+  local name
+  local value
+  local timestamp
+
+  metric_name=$1
+  repo=$2
+  name=$3
+  value=$4
+  timestamp=$5
+
+  if [[ $repo == 'all' && $name == 'all' ]]; then
+    put_metric_data_dimension "$metric_name" "$value" "$timestamp"
+  elif [[ $repo == 'all' && $name != 'all' ]]; then
+    put_metric_data_dimension "$metric_name" "$value" "$timestamp" "WorkflowName=$name"
+  elif [[ $repo != 'all' && $name == 'all' ]]; then
+    put_metric_data_dimension "$metric_name" "$value" "$timestamp" "Repo=$repo"
+  elif [[ $repo != 'all' && $name != 'all' ]]; then
+    put_metric_data_dimension "$metric_name" "$value" "$timestamp" "Repo=$repo,WorkflowName=$name"
+  fi
+}
+
+main() {
+  round_arg=""
+  verbose_arg=""
+
+  while getopts "cdi:rv" opt; do
+      case $opt in
+          c)
+              CLOUDWATCH=1
+              ;;
+          d)
+              DRYRUN=1
+              ;;
+          i)
+              INTERVAL=${OPTARG}
+              ;;
+          r)
+              round_arg="-r"
+              ;;
+          v)
+              verbose_arg="-vvv"
+              ;;
+          :)
+              echo "Error: option ${OPTARG} requires an argument"
+              ;;
+          ?)
+              echo "Invalid option: ${OPTARG}" >&2
+              echo >&2
+              usage >&2
+              exit 1
+              ;;
+      esac
+  done
+
+  shift $((OPTIND-1))
+  if [[ -z $INTERVAL ]]; then
+    usage >&2
+    exit 1
+  fi
+
+  if [[ $CLOUDWATCH == 0 ]]; then
+    python3 "$BASEDIR"/github-workflow-monitor.py --interval "$INTERVAL" $round_arg $verbose_arg "$@"
+  else
+    IFS=$'\n'
+    workflow_stats=($(python3 "$BASEDIR"/github-workflow-monitor.py --interval "$INTERVAL" $round_arg $verbose_arg "$@"))
+    unset IFS
+
+    num_stats=${#workflow_stats[@]}
+    for (( i=0; i<num_stats; i++ )); do
+      echo "${workflow_stats[i]}"
+      [[ $i == 0 ]] && continue
+
+      IFS=','
+      workflow_stat=(${workflow_stats[i]})
+      unset IFS
+
+      timestamp=${workflow_stat[0]}
+      repo=${workflow_stat[1]}
+      name=${workflow_stat[2]}
+      num_success=${workflow_stat[3]}
+      num_failed=${workflow_stat[4]}
+      put_metric_data "GitHubActionRunsSuccessCount" "$repo" "$name" "$num_success" "$timestamp"
+      put_metric_data "GitHubActionRunsFailedCount" "$repo" "$name" "$num_failed" "$timestamp"
+    done
+  fi
+}
+
+main "$@"
