cda4f6a002705f9dfcb0c82e148fd3525ecf4fd0
Dominic Robinson <dominic.robinson@digital.justice.gov.uk>
2025-01-17 10:50:29 +0000

    -

--- src/run_script_on_ec2.sh
+++ src/run_script_on_ec2.sh
@@ -1,21 +1,28 @@
 #!/bin/bash
 set -eo pipefail
 
+SHOW_PROGRESS="${SHOW_PROGRESS:-1}"
 SHOW_COMMAND_JSON="${SHOW_COMMAND_JSON:-0}"
 SHOW_RESULT_JSON="${SHOW_RESULT_JSON:-0}"
 SHOW_STDERR_ON_SUCCESS="${SHOW_STDERR_ON_SUCCESS:0}"
 TIMEOUT_SECS="${TIMEOUT_SECS:-300}"
 CHECK_INTERVAL_SECS="${CHECK_INTERVAL_SECS:-10}"
+LOGPREFIX=
 
 usage() {
-    echo "Usage: $0 first|all <tag:Name> <comment> <script|filename>"
+    echo "Usage (1): $0 first|all        <tag:Name>                     <comment> <script|filename> [logprefix]"
+    echo "Usage (2): $0 shell|powershell 'instance_id1 .. instance_idn' <comment> <script|filename> [logprefix]"
     echo
-    echo "Finds EC2s matching tag:Name and runs the given script using SSM"
+    echo "Usage (1) Finds EC2s matching tag:Name and runs the given script using SSM"
     echo "  first: run on first EC2 only."
     echo "  all:   run on all matching EC2s"
+    echo
+    echo "Usage (2) Runs given shell or powershell script against all given instance ids"
+    echo
     echo "Returns exitcode of 1 if script fails to run, 2 if instance not found"
     echo
-    echo "Example Usage: TIMEOUT_SECS=3600 $0 first 'pd-ndh-app-a' 'cdecopy' 'sudo su tibco -c cdecopy.sh'"
+    echo "Example Usage: TIMEOUT_SECS=3600 $0 first 'pd-ndh-app-a'        'cdecopy'                  'sudo su tibco -c cdecopy.sh'"
+    echo "Example Usage:                   $0 all   'server-type:ncr-web' 'systemctl status sapbobj' 'sudo systemctl status sapbobj'"
 }
 
 get_command_json() {
@@ -44,11 +51,13 @@ get_command_json() {
 run_command_on_instance() {
   local command_json
   command_json=$(get_command_json "$3")
-  echo "aws ssm send-command \
-    --instance-ids '$1' \
-    --document-name '$4' \
-    --comment '$2' \
-    --parameters '$3'" >&2
+  if ((SHOW_PROGRESS != 0)); then
+    echo "${LOGPREFIX}aws ssm send-command \
+      --instance-ids '$1' \
+      --document-name '$4' \
+      --comment '$2' \
+      --parameters '$3'" >&2
+  fi
   aws ssm send-command \
     --instance-ids $1 \
     --document-name "$4" \
@@ -65,7 +74,9 @@ wait_for_command() {
   n=$((1+TIMEOUT_SECS/CHECK_INTERVAL_SECS))
   for i in $(seq 1 $n); do
     sleep "$CHECK_INTERVAL_SECS"
-    echo "[$i/$n] aws ssm get-command-invocation --instance-id '$1' --command-id '$2'" >&2
+    if ((SHOW_PROGRESS != 0)); then
+      echo "${LOGPREFIX}[$i/$n] aws ssm get-command-invocation --instance-id '$1' --command-id '$2'" >&2
+    fi
     result_json=$(aws ssm get-command-invocation --instance-id "$1" --command-id "$2")
     result_status=$(jq -r ".Status" <<< "$result_json")
     if [[ $result_status == "Success" || $result_status == "Failed" || $result_status == "Terminated" ]]; then
@@ -76,31 +87,49 @@ wait_for_command() {
 }
 
 main() {
-  instance_count=$1
-  name=$2
+  option=$1
   comment=$3
   script=$4
-  if [[ -z $instance_count || -z $name || -z $comment || -z $script ]]; then
+  LOGPREFIX=$5
+  if [[ -z $option || -z $comment || -z $script ]]; then
     usage >&2
     exit 1
   fi
 
-  instance_json=$(aws ec2 describe-instances --no-cli-pager --filters "Name=tag:Name,Values=$name" "Name=instance-state-name,Values=running" || true)
-  instance_ids=$(jq -r ".Reservations[].Instances[].InstanceId" <<< "$instance_json")
-  platforms=$(jq -r ".Reservations[].Instances[].Platform" <<< "$instance_json" | sort -u)
-  if [[ $platforms == "windows" ]]; then
+  if [[ $option == "shell" ]]; then
+    document_name=AWS-RunShellScript
+    instance_ids="$2"
+  elif [[ $option == "powershell" ]]; then
     document_name=AWS-RunPowerShellScript
+    instance_ids="$2"
+  elif [[ $option == "all" || $option == "first" ]]; then
+    if [[ $2 == *:* ]]; then
+      tag=${2%:*}
+      name=${2#*:}
+    else
+      tag=Name
+      name=$2
+    fi
+    if [[ -z $name || -z $tag ]]; then
+      usage >&2
+      exit 1
+    fi
+    instance_json=$(aws ec2 describe-instances --no-cli-pager --filters "Name=tag:$tag,Values=$name" "Name=instance-state-name,Values=running" || true)
+    instance_ids=$(jq -r ".Reservations[].Instances[].InstanceId" <<< "$instance_json")
+    platforms=$(jq -r ".Reservations[].Instances[].Platform" <<< "$instance_json" | sort -u)
+    if [[ $platforms == "windows" ]]; then
+      document_name=AWS-RunPowerShellScript
+    else
+      document_name=AWS-RunShellScript
+    fi
+    if [[ -z $instance_ids ]]; then
+      echo "${LOGPREFIX}No running instance with tag:$tag=$name" >&2
+      exit 2
+    fi
+    if [[ $option == "first" ]]; then
+      instance_ids=$(echo "$instance_ids" | head -1)
+    fi
   else
-    document_name="AWS-RunShellScript"
-  fi
-  if [[ -z $instance_ids ]]; then
-    echo "No running instance with tag:Name=$name" >&2
-    exit 2
-  fi
-  if [[ $instance_count == "first" ]]; then
-    instance_ids=$(echo "$instance_ids" | head -1)
-  elif [[ $instance_count != "all" ]]; then
-    echo "Expected first or all, got $instance_count" >&2
     usage >&2
     exit 1
   fi
@@ -118,19 +147,23 @@ main() {
     fi
     result_status=$(jq -r ".Status" <<< "$result_json")
 
-    echo "Results for instance_id=$instance_id" >&2
+    if ((SHOW_PROGRESS != 0)); then
+      echo "${LOGPREFIX}Results for instance_id=$instance_id" >&2
+    fi
     if [[ $result_status == "Success" ]]; then
       if ((SHOW_STDERR_ON_SUCCESS != 0)); then
-        jq -r ".StandardErrorContent" <<< "$result_json" >&2
+        jq -r ".StandardErrorContent" <<< "$result_json" | sed -e "s/^/$LOGPREFIX/" >&2
       fi
-      jq -r ".StandardOutputContent" <<< "$result_json"
+      jq -r ".StandardOutputContent" <<< "$result_json" | sed -e "s/^/$LOGPREFIX/"
     elif [[ $result_status == "Failed" ]]; then
-      jq -r ".StandardErrorContent" <<< "$result_json" >&2
-      jq -r ".StandardOutputContent" <<< "$result_json"
+      jq -r ".StandardErrorContent" <<< "$result_json" | sed -e "s/^/$LOGPREFIX/" >&2
+      jq -r ".StandardOutputContent" <<< "$result_json" | sed -e "s/^/$LOGPREFIX/"
       exitcode=1
     else
-      echo "Command failed to execute, or timed out waiting for response >&2"
-      echo "$result_status" >&2
+      if ((SHOW_PROGRESS != 0)); then
+        echo "${LOGPREFIX}Command failed to execute, or timed out waiting for response >&2"
+      fi
+      echo "${LOGPREFIX}$result_status" >&2
       exitcode=1
     fi
   done
