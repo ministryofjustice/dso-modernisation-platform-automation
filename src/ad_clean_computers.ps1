dde6eee9095a96bf6ef6723d432c69767c641532
Dave Kent <dave.kent@justice.gov.uk>
2025-05-19 17:12:17 +0100

    tm-1036-clean-ad-computers-pipeline-mon5

--- src/ad_clean_computers.ps1
+++ src/ad_clean_computers.ps1
@@ -66,7 +66,7 @@ import-Module -Name AWSPowerShell -MinimumVersion 4.1.807
 
 # Get the secret value
 $hostname = (Get-ComputerInfo).CsName
-$secretValue = Get-SECSecretValue -SecretId "/$hostname/dso-modernisation-platform-automation" -Region "eu-west-2"
+$secretValue = Get-SECSecretValue -SecretId "/t1-jump2022-1/dso-modernisation-platform-automation" -Region "eu-west-2"
 $raw = $secretValue.SecretString.Trim('{}').Trim()
 $parts = $raw -split ',\s*'
 
