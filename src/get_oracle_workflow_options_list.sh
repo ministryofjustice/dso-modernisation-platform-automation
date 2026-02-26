64bd19e84758a71d155b4468dcbd451d69bb98dc
bill-buchan <55987520+bill-buchan@users.noreply.github.com>
2026-02-26 17:58:43 +0000

    Support Running Ansible Against Database Systems as well as Primary Databases (#212)
    * Consolidate scripts
    
    * interim
    
    * New Sys Get Name Workflow
    
    * Fix typo
    
    * Supporting batching of target pairs
    
    Avoid running Ansible playbook against the same host more than once at a time
    
    * batch_counter is a variable
    
    * Minor tidy up
    
    * Fix batch counting
    
    * Create batches using JSON
    
    * Output should now be an array of arrays
    
    * Convert colon-separated elements into JSON dictionaries
    
    * Use simple key-value pairs
    
    * revert for debug
    
    * try again
    
    * fix typo
    
    * debug
    
    * debug
    
    * debug
    
    * debug
    
    * debug
    
    * debug
    
    * debug
    
    * debug
    
    * debug
    
    * debug
    
    * decode
    
    * debug
    
    * Try to workaround github actions
    
    * More debug
    
    * debug
    
    * Apply encoding to JSON
    
    * debug
    
    * Missing dollar
    
    * view the json
    
    * debug
    
    * Spool from file
    
    * jq
    
    * github actions cannot be thwarted
    
    * Display
    
    * Use base64 encoding to hide content from Github actions
    
    * Echo all parameters
    
    * Use jq to process json
    
    * Can we see it
    
    * Loop through batches
    
    * Set outputs
    
    * Decode the JSON
    
    * jq batches are zero offset
    
    * Batch must use a JQ argument
    
    * Make output compact
    
    * Apply processing limits
    
    * Show the target group
    
    * Fix variable names
    
    * Use correct variable
    
    * Use correct target group for Delius applications
    
    * Remove closers
    
    * Fix application pattern match
    
    * Use common setup action for primary and system targets
    
    * Use correct variable name
    
    * Include application type for Delius environments
    
    * Remove repetitive code
    
    * Push event no longer required
    
    * Expect only a single primary database
    
    * OEM app uses underscores
    
    * debug
    
    * Special handling of the HMPPS OEM environment
    
    * Add missing HMPPS OEM targets
    
    * Special handling of OEM
    
    * Encode OEM hosts
    
    * debug
    
    * format batch as json
    
    * remove debug
    
    * Use new system access for SQL Plan Baselines
    
    * Remove obsolete version of code
    
    * Supply the application name
    
    * Add execute permission
    
    * Use consistent plurality
    
    * Supply application name
    
    * DDL Notifier not yet required
    
    * Remove test files
    
    * Fix variable case
    
    * Sort service names
    
    * Remove redundant conditional

--- src/get_oracle_workflow_options_list.sh
+++ src/get_oracle_workflow_options_list.sh
@@ -1,61 +1,118 @@
 #!/bin/bash
+#
+# Generate lists of Oracle workflow options:
+#  - primary databases
+#  - database systems (i.e. primary and all standby databases)
 
-# List of applications supported by DBAs
-DBA_APPLICATIONS_LIST=$1
+set -euo pipefail
 
-# Path to the Ansible Group Vars files directory
-GROUP_VARS_DIRECTORY=$2
+usage() {
+   echo "Usage: $0 [primary|system] <dba_applications_list> <group_vars_directory>"
+}
 
-if [[ ! -d ${GROUP_VARS_DIRECTORY} ]];
-then
+if [[ $# -ne 3 ]]; then
+   usage
+   exit 1
+fi
+
+MODE=$1
+DBA_APPLICATIONS_LIST=$2
+GROUP_VARS_DIRECTORY=$3
+
+if [[ ! -d ${GROUP_VARS_DIRECTORY} ]]; then
    echo "Ansible group_vars directory not found at ${GROUP_VARS_DIRECTORY}."
    exit 1
 fi
 
-# Create array of all possible target primary databases
-declare -a TARGET_DATABASES
+if [[ ! -f ${DBA_APPLICATIONS_LIST} ]]; then
+   echo "DBA applications list not found at ${DBA_APPLICATIONS_LIST}."
+   exit 1
+fi
 
-for GROUP_VARS_FILE in $(ls -1 ${GROUP_VARS_DIRECTORY} | grep  -f <(sed 's/.*/environment_name_&_.*.yml/' ${DBA_APPLICATIONS_LIST} | sed 's/-/_/g' ));
+case "${MODE}" in
+   primary|system)
+      ;;
+   *)
+      usage
+      exit 1
+      ;;
+esac
+
+# Create array of all possible target entries
+declare -a TARGET_ENTRIES
+
+for GROUP_VARS_FILE in $(ls -1 ${GROUP_VARS_DIRECTORY} | grep -f <(sed 's/.*/environment_name_&_.*.yml/' ${DBA_APPLICATIONS_LIST} | sed 's/-/_/g'));
 do
-   # For Delius (delius-core and delius-mis) we are only interested in the primary databases hosts
-   # and we do not need the database name as there is only one per host
-   if [[ "$GROUP_VARS_FILE" == *"delius"* ]];
-   then 
-      if [[ "$GROUP_VARS_FILE" == *"_primarydb.yml" ]];
-      then
-        TARGET_DATABASES+=("$(echo ${GROUP_VARS_FILE%%_primarydb.yml} | sed 's/^environment_name_//')")
+   # For Delius (delius-core and delius-mis) we simply refer to the primarydb as there is only ever exactly one
+   # of those per application group - the entry is the same regardless if it is a primary or system mode
+   if [[ "${GROUP_VARS_FILE}" == *"delius"* ]]; then
+      if [[ "${GROUP_VARS_FILE}" == *"_primarydb.yml" ]]; then
+         TARGET_ENTRIES+=("$(echo ${GROUP_VARS_FILE%%_primarydb.yml} | sed 's/^environment_name_//')")
       fi
+   elif [[ "${GROUP_VARS_FILE}" == *"hmpps_oem"* ]]; then
+       # For OEM we refer to the primary EMREP and RCVCAT databases as there are only ever these 2
+       # primary databases in the environment - the entry is the same regardless if it is a primary or system mode 
+       TARGET_ENTRIES+=("$(echo ${GROUP_VARS_FILE#*environment_name_} | sed 's/.yml$//') EMREP")
+       TARGET_ENTRIES+=("$(echo ${GROUP_VARS_FILE#*environment_name_} | sed 's/.yml$//') RCVCAT")
    else
-      # For other applications find all the databases listed in the environment
-      for DATABASE in $(yq '.db_configs? | select(. != null) | keys | .[]' ${GROUP_VARS_DIRECTORY}/${GROUP_VARS_FILE})
-      do
-         # The RCVCAT database is referenced in all non-Delius environments but it is
-         # only a real database in the hmpps_oem application environments so exclude it elsewhere
-         if [[ "$DATABASE" == "RCVCAT" ]];
-         then
-            if [[ "$GROUP_VARS_FILE" == *"hmpps_oem"* ]];
-            then
-               TARGET_DATABASES+=("$(echo ${GROUP_VARS_FILE#*environment_name_} | sed 's/.yml$//') $DATABASE")
+      # Non-Delius applications...
+      if [[ "${MODE}" == "primary" ]]; then
+         # For other applications find all the databases listed in the environment
+         for DATABASE in $(yq '.db_configs? | select(. != null) | keys | .[]' ${GROUP_VARS_DIRECTORY}/${GROUP_VARS_FILE});
+         do
+            # The RCVCAT database is referenced in all non-Delius environments but it is
+            # only a real database in the hmpps_oem application environments so exclude it elsewhere
+            if [[ "${DATABASE}" != "RCVCAT" ]]; then
+               if [[ ! "${DATABASE}" == "DR"* ]]; then
+                  # Non-Delius standby databases are identified by the DR prefix. Ignore those.
+                  TARGET_ENTRIES+=("$(echo ${GROUP_VARS_FILE#*environment_name_} | sed 's/.yml$//') ${DATABASE}")
+               fi
             fi
-         else      
-            if [[ ! "$DATABASE" == "DR"* ]];
-            then
-               # Non-Delius standby databases are identified by the DR prefix.  Ignore those.
-               TARGET_DATABASES+=("$(echo ${GROUP_VARS_FILE#*environment_name_} | sed 's/.yml$//') $DATABASE")
-            fi
+         done
+      else
+         # mode == system (get all databases; regardless of whether they are a primary or standby)
+         if yq 'has("db_configs")' ${GROUP_VARS_DIRECTORY}/${GROUP_VARS_FILE} | grep -q true; then
+            # We ignore entries which do not have a host_name as they will correspond to either obsolete databases
+            # or the RMAN catalog database, which only exists in hmpps-oem* environment
+            for SYSTEM in $(
+               yq -o=json '.db_configs' ${GROUP_VARS_DIRECTORY}/${GROUP_VARS_FILE} \
+                              | jq -r '
+                              to_entries
+                              | map(
+                                 select(.value.host_name != null)
+                                 | {key, services: (.value.service // [] | map(.name))}
+                              )
+                              | map(
+                                 .services[] as $svc
+                                 | {service:$svc, key:.key}
+                              )
+                              | sort_by(.service)
+                              | group_by(.service)
+                              | map("(\(.[0].service))=>\(map(.key) | join(","))")
+                              | .[]'
+                           );
+            do
+               TARGET_ENTRIES+=("$(echo ${GROUP_VARS_FILE#*environment_name_} | sed 's/.yml$//') ${SYSTEM}")
+            done
          fi
-      done
+      fi
    fi
 done
 
-# Get longest string length so we can format the array 
+# Get longest string length so we can format the array
 MAX_LENGTH=0
-for s in "${TARGET_DATABASES[@]} ";
+for s in "${TARGET_ENTRIES[@]}";
 do
-    (( ${#s} > MAX_LENGTH )) && MAX_LENGTH=${#s}
+   (( ${#s} > MAX_LENGTH )) && MAX_LENGTH=${#s}
 done
 
-echo ">>>>>>>>CUT HERE FOR TARGET DATABASE LIST>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
-# Print all target databases in alphabetical order formatted for align together
-printf '%s\n' "${TARGET_DATABASES[@]}" | sort | awk -v MAX=${MAX_LENGTH} '{PAD=2+MAX-length($1 $2); printf("          - '"'"'%s%*s%s'"'"'\n",$1,PAD,"",$2)}' 
-echo "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<"
+if [[ "${MODE}" == "primary" ]]; then
+   echo ">>>>>>>>CUT HERE FOR TARGET DATABASE LIST>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
+else
+   echo ">>>>>>>>CUT HERE FOR TARGET DATABASE SYSTEMS LIST>>>>>>>>>>>>>>>>>>>>>>>"
+fi
+
+# Print all target entries in alphabetical order formatted for align together
+printf '%s\n' "${TARGET_ENTRIES[@]}" | sort | awk -v MAX=${MAX_LENGTH} '{PAD=2+MAX-length($1 $2); printf("          - '\''%s%*s%s'\''\n",$1,PAD,"",$2)}'
+
+echo "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<"
