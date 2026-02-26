7203d13a71ffd29939f81fc8e1cb7bcb0edaec98
Bill Buchan <bill.buchan@digital.justice.gov.uk>
2026-02-26 16:53:03 +0000

    Remove test files

--- src/resolve.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
- jq -r --arg ip "$1" '
-  ._meta.hostvars
-  | to_entries
-  | map(select(.value.network_interfaces[]?.private_ip_address == $ip))
-  | .[].key
-' < <(ansible-inventory -i /inventory --list)
