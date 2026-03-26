89075bde6a2985851a7b10e497f5d5a732c91523
Dave Kent <dave.kent@justice.gov.uk>
2026-03-26 17:48:14 +0000

    tm-1906-fix-credentials-ad-user-cleanup-script-2

--- src/ad_clean_users.ps1
+++ src/ad_clean_users.ps1
@@ -97,16 +97,6 @@ $errorCount = 0
 
 # Get the secret value
 # import-Module -Name AWSPowerShell -MinimumVersion 4.1.807
-# $hostname = (Get-ComputerInfo).CsName
-# $adSecretValue = Get-SECSecretValue -SecretId "/$($hostname.ToLower())/dso-ad-computer-cleanup" -Region "eu-west-2"
-# $adSecretValue = $adSecretValue.SecretString | ConvertFrom-Json
-# $username = $adSecretValue.username
-# $password = $adSecretValue.password
-# $domainname = $adSecretValue.domainname
-# $password = ConvertTo-SecureString -String $password -AsPlainText -Force
-
-# $adcred = New-Object System.Management.Automation.PSCredential ($username, $Password)
-
 Import-Module -Name AWS.Tools.SecretsManager
 $hostname = (Get-ComputerInfo).CsName
 $adSecretValue = Get-SECSecretValue -SecretId "/$($hostname.ToLower())/dso-ad-computer-cleanup" -Region "eu-west-2"
